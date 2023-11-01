3d1b8b225c7e23620cd5a0e0275d73c4565b53fd
Сапожников Вячеслав <sapognikovslava@gmail.com>
2023-11-01 23:41:50 +0300

    heroStats cubit fix class modifiers

--- lib/features/hero_stats/hero_stats_state.dart
+++ lib/features/hero_stats/hero_stats_state.dart
@@ -1,22 +1,22 @@
 import 'package:lecture_5/domain/model/hero_stats_model.dart';
 
-abstract class HeroStatsState {}
+sealed class HeroStatsState {}
 
-abstract class HeroStatsBuildState extends HeroStatsState {}
+final class HeroStatsBuildState extends HeroStatsState {}
 
-abstract class HeroStatsListenState extends HeroStatsState {}
+final class HeroStatsListenState extends HeroStatsState {}
 
-class HeroStatsInitial extends HeroStatsBuildState {}
+final class HeroStatsInitial extends HeroStatsBuildState {}
 
-class HeroStatsLoading extends HeroStatsBuildState {}
+final class HeroStatsLoading extends HeroStatsBuildState {}
 
-class HeroStatsError extends HeroStatsListenState {
+final class HeroStatsError extends HeroStatsListenState {
   final String error;
 
   HeroStatsError({required this.error});
 }
 
-class HeroStatsData extends HeroStatsBuildState {
+final class HeroStatsData extends HeroStatsBuildState {
   final List<HeroStatsModel> heroStats;
 
   HeroStatsData({required this.heroStats});
